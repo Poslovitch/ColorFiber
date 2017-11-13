5e640d006e314c3d78be2bfe3c34021a4798cd50
Florian CUNY <poslovitch@gmail.com>
2017-11-13 14:56:41 +0100

    THIS IS WORKING.

---
+++ src/fr/poslovitch/colorfiber/ColorFiber.java
@@ -0,0 +1,14 @@
+package fr.poslovitch.colorfiber;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ColorFiber extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setHeight(300);
+        primaryStage.setWidth(300);
+        primaryStage.show();
+    }
+}
